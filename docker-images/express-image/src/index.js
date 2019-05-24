2720d18e248d8aa278be13066b9da1041a948498
David Cruchon <david@enophi.ch>
2019-05-24 21:46:31 +0200

    Final touch to our express server

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,12 +1,19 @@
-const Chance = require('chance');
-var chance = new Chance();
 var express = require('express');
 var app = express();
+var faker = require('faker');
 
-app.get('/', function(req, res) {
-    res.send("Hello from RES");
-});
+app.get('/', generateRandomData );
 
 app.listen(3000, function() {
     console.log("Accepting HTTP request on port 3000");
 });
+
+function generateRandomData( req , res ) {
+
+    // Randomize the response
+    if(Math.random() >= 0.5)
+        res.status(200).send(faker.helpers.createCard());
+    else
+        res.status(401).send({"status":"You are not authorize!"})
+
+}
